78cee15dea92b3fc4449200152f44e420a8b93d4
Sergii Butenko <sergii.butenko@globallogic.com>
2019-05-07 20:01:26 +0300

    modified structure

---
+++ runSensors.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export PYTHONPATH=${PYTHONPATH}:./
+python3 -m sensors.app
--- runWeb.sh
+++ runWeb.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 export PYTHONPATH=${PYTHONPATH}:./
-export PYTHONPATH=${PYTHONPATH}:./backend
 python3 -m web.app
