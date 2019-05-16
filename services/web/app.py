47be323cdeec6b2fbbcf5b997f536484ba1301da
Sergii Butenko <sergii.butenko@globallogic.com>
2019-05-16 11:59:47 +0300

    changes to socketio

--- services/web/app.py
+++ services/web/app.py
@@ -13,5 +13,10 @@ app.register_blueprint(devices, url_prefix="/v1/devices")
 app.register_blueprint(auth, url_prefix="/v1/auth")
 app.register_blueprint(groups, url_prefix="/v1/groups")
 
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", debug=True)
